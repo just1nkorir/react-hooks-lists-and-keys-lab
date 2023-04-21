3d9b620a424f09bd8eac3c420e7b51bf5b757fea
just1nkorir <justin.korir@student.moringaschool.com>
2023-04-21 16:41:24 +0300

    .

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,11 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem name= "Reciplease" about ="A recipe tracking app" technologies ={['Rails', 'Bootstrap CSS']}/>
+        <ProjectItem name= "Kibbles N Bitz" about ="Tinder for dogs" technologies ={['React', 'Redux']}/>
+        <ProjectItem name= "Alienwares" about ="Etsy for aliens" technologies ={['React', 'Redux', 'Rails']}/>
+      </div>
     </div>
   );
 }
