f2e44b3eaf79b08aaa06ccdd30ad03892c32377b
just1nkorir <justin.korir@student.moringaschool.com>
2023-04-24 11:29:15 +0300

    Completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  const lang = technologies.map((technology)=> {<span key={technology}>{technology}</span>})
+  const languages = technologies.map((technology)=> (<span key={technology}>{technology}</span>))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {lang}
+        {languages}
       </div>
     </div>
   );
